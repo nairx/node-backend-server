9ff421297ee778f28356a4c0d7129784fba27f31
nairx <nairxprv@gmail.com>
2026-08-26 16:01:30 +0530

    updated

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(8081, () => console.log("Server started"))
+
+//create the folder node-backend-server
+//open the folder in vscode
+//in terminal type npm init -y
+//in package.json change the type to module
+//create server.js file
+//node server.js
